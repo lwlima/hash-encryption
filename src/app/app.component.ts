8cf3d51e6824201efb0a8937ac1b5a581b1ac1aa
Luciano W. Lima <contato@lucianolima.dev>
2022-05-05 07:53:10 -0300

    Add hash encryption Sha-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Md5 } from 'ts-md5';
+import { sha256, sha224 } from 'js-sha256';
+import { sha384,sha512, sha512_224, sha512_256 } from 'js-sha512';
 
 declare let require: any;
 
@@ -13,25 +15,36 @@ export class AppComponent {
   title = 'HASH-ENCRYPTION';
   text: string = '';
   type: string = '';
-  hash: string | Int32Array = '';
-
+  hashMd5: string | Int32Array = '';
+  hashSha1: string = '';
+  hashSha2_224: string = '';
+  hashSha2_256: string = '';
+  hashSha2_384: string = '';
+  hashSha2_512: string = '';
+  hashSha2_512_224: string = '';
+  hashSha2_512_256: string = '';
+  
   encrypt = (type: string): string => {
     let md5 = () => {
       const md5 = new Md5();
-      this.hash = md5.appendStr(this.text).end();
+      this.hashMd5 = md5.appendStr(this.text).end();
     }
 
     let sha1 = () => {
       let sha1 = require('sha1');
-      this.hash = sha1(this.text);
+      this.hashSha1 = sha1(this.text);
     }
 
     let sha2 = () => {
-      return this.hash = '';
+      this.hashSha2_224 = sha224(this.text);
+      this.hashSha2_256 = sha256(this.text);
+      this.hashSha2_384 = sha384(this.text);
+      this.hashSha2_512 = sha512(this.text);
+      this.hashSha2_512_224 = sha512_224(this.text);
+      this.hashSha2_512_256 = sha512_256(this.text);
     }
 
     let sha3 = () => {
-      return this.hash = '';
     }
 
     var methods: any = {
