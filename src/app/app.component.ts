ced24fee2aee2a480f942845952017f7d4d7d503
Luciano W. Lima <contato@lucianolima.dev>
2022-05-05 07:18:13 -0300

    Add hash encryption SHA-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Md5 } from 'ts-md5';
 
+declare let require: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,35 +16,25 @@ export class AppComponent {
   hash: string | Int32Array = '';
 
   encrypt = (type: string): string => {
-    let md2 = (): string => {
-      return this.hash = '';
-    }
-
-    let md4 = (): string => {
-      return this.hash = '';
-    }
-
-    let md5 = (): string|Int32Array => {
+    let md5 = () => {
       const md5 = new Md5();
       this.hash = md5.appendStr(this.text).end();
-      return this.hash;
     }
 
-    let sha1 = (): string => {
+    let sha1 = () => {
+      let sha1 = require('sha1');
+      this.hash = sha1(this.text);
+    }
+
+    let sha2 = () => {
       return this.hash = '';
     }
 
-    let sha2 = (): string => {
-      return this.hash = '';
-    }
-
-    let sha3 = (): string => {
+    let sha3 = () => {
       return this.hash = '';
     }
 
     var methods: any = {
-      'md2':  md2,
-      'md4':  md4,
       'md5':  md5,
       'sha1': sha1,
       'sha2': sha2,
